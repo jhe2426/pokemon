cdbf5816e58069104eb22bd5037f31d50b99c729
jhe2426 <jhe2426@naver.com>
2025-05-24 22:34:00 +0900

    26 - NPC 추가

--- Pokemon/Assets/Scripts/Character/PlayerController.cs
+++ Pokemon/Assets/Scripts/Character/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public LayerMask solidObjectsLayer;
+    public LayerMask interactabLayer;
     public LayerMask grassLayer;
 
     public event Action OnEncountered;
@@ -44,6 +45,23 @@ public class PlayerController : MonoBehaviour
         }
 
         animator.SetBool("isMoving", isMoving);
+
+        if (Input.GetKeyDown(KeyCode.Z))
+            Interact();
+    }
+
+    void Interact()
+    {
+        var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
+        var interactPos = transform.position + facingDir;
+
+        //Debug.DrawLine(transform.position, interactPos, Color.green, 0.5f);
+
+        var collider = Physics2D.OverlapCircle(interactPos, 0.3f, interactabLayer);
+        if (collider != null)
+        {
+            collider.GetComponent<Interactable>()?.Interact();
+        }
     }
 
     IEnumerator Move(Vector3 targetPos) 
@@ -64,7 +82,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsWalkalbe(Vector3 targetPos) 
     {
-        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer) != null) 
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer | interactabLayer) != null) 
         {
             return false;
         }
