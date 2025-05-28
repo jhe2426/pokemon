2464af6601f5f676438e1893ccf72f73a7abfd58
jhe2426 <jhe2426@naver.com>
2025-05-28 18:42:04 +0900

    41 - 두번째 씬 생성 및 포털 이동 기능 구현

--- Pokemon/Assets/Scripts/Character/Character.cs
+++ Pokemon/Assets/Scripts/Character/Character.cs
@@ -7,11 +7,23 @@ public class Character : MonoBehaviour
 
     public bool IsMoving { get; private set; }
 
+    public float OffsetY { get; private set; } = 0.3f;
+
     CharacterAnimator animator;
 
     private void Awake()
     {
         animator = GetComponent<CharacterAnimator>();
+        SetPositionAndSnapToTile(transform.position);
+    }
+
+    public void SetPositionAndSnapToTile(Vector2 pos)
+    {
+        // 2.3 -> Floor() -> 2 -> 2.5
+        pos.x = Mathf.Floor(pos.x) + 0.5f;
+        pos.y = Mathf.Floor(pos.y) + 0.5f + OffsetY;
+
+        transform.position = pos;
     }
 
     public IEnumerator Move(Vector2 moveVec, Action OnMoveOver = null)
--- Pokemon/Assets/Scripts/SceneManagement/Portal.cs
+++ Pokemon/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,9 +1,31 @@
+using System.Collections;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour, IPlayerTriggerable
 {
+    [SerializeField] int sceneToLoad = -1;
+    [SerializeField] Transform spawnPoint;
+
+    PlayerController player;
     public void OnPlayerTriggered(PlayerController player)
     {
-        Debug.Log("player enterd the portal");
+        this.player = player;
+        StartCoroutine(SwitchScene());
     }
+
+    IEnumerator SwitchScene()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+        var destPortal = FindObjectsOfType<Portal>().First(x => x != this);
+        player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
+
+        Destroy(gameObject);
+    }
+
+    public Transform SpawnPoint => spawnPoint;
 }
