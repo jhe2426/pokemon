a33b647c7a0b471381126f51f327ea84423182db
jhe2426 <jhe2426@naver.com>
2025-05-29 00:42:22 +0900

    43 - 집 내부 씬 구현, Fading 기능 구현

--- Pokemon/Assets/Scripts/SceneManagement/Portal.cs
+++ Pokemon/Assets/Scripts/SceneManagement/Portal.cs
@@ -16,17 +16,25 @@ public class Portal : MonoBehaviour, IPlayerTriggerable
         StartCoroutine(SwitchScene());
     }
 
+    Fader fader;
+    private void Start()
+    {
+        fader = FindObjectOfType<Fader>();
+    }
+
     IEnumerator SwitchScene()
     {
         DontDestroyOnLoad(gameObject);
 
         GameController.Instance.PauseGame(true);
+        yield return fader.FadeIn(0.5f);
 
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
         var destPortal = FindObjectsOfType<Portal>().First(x => x != this && x.destinationPortal == this.destinationPortal);
         player.Character.SetPositionAndSnapToTile(destPortal.SpawnPoint.position);
-
+        
+        yield return fader.FadeOut(0.5f);
         GameController.Instance.PauseGame(false);
 
         Destroy(gameObject);
