9d0d408b4bc008bdc5d128324a25ea9259c5ab97
jhe2426 <jhe2426@naver.com>
2025-05-30 23:05:17 +0900

    58 - 가방 아이템 종류 추가하기

---
+++ Pokemon/Assets/Scripts/Inventory/PokeballItem.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Items/Create new pokeball")]
+public class PokeballItem : ItemBase
+{
+    public override bool Use(Pokemon pokemon)
+    {
+        return true;
+    }
+}
