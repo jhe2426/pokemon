580081295be6a7868bd404ee4da48a21dbbd6aad
jhe2426 <jhe2426@naver.com>
2025-05-31 06:36:29 +0900

    68 - NPC 퀘스트 기능 구현

--- Pokemon/Assets/Scripts/Inventory/Inventory.cs
+++ Pokemon/Assets/Scripts/Inventory/Inventory.cs
@@ -44,7 +44,7 @@ public class Inventory : MonoBehaviour, ISavable
         if (itemUsed)
         {
             if (!item.IsReusable)
-                RemoveItem(item, selectedCategory);
+                RemoveItem(item);
                 
             return item;
         }
@@ -74,8 +74,9 @@ public class Inventory : MonoBehaviour, ISavable
         OnUpdated?.Invoke();
     }
 
-    public void RemoveItem(ItemBase item, int category)
+    public void RemoveItem(ItemBase item)
     {
+        int category = (int)GetCategoryFromItem(item);
         var currentSlots = GetSlotsByCategory(category);
 
         var itemSlot = currentSlots.First(slot => slot.Item == item);
@@ -84,6 +85,14 @@ public class Inventory : MonoBehaviour, ISavable
             currentSlots.Remove(itemSlot);
 
         OnUpdated?.Invoke();
+    }
+
+    public bool HasItem(ItemBase item)
+    {
+        int category = (int)GetCategoryFromItem(item);
+        var currentSlots = GetSlotsByCategory(category);
+
+        return currentSlots.Exists(slot => slot.Item == item);
     }
 
     ItemCategory GetCategoryFromItem(ItemBase item)
